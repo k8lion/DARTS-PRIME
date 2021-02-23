7378d8ca16a1b5ceb79dd1441ad50774d672c843
k8lion <kaitlinmaile@gmail.com>
2021-02-23 12:42:53 +0100

    eval bath

--- src/genotypes.py
+++ src/genotypes.py
@@ -93,3 +93,6 @@ DARTS_V2 = Genotype(
             ('skip_connect', 2), ('skip_connect', 2), ('max_pool_3x3', 1)], reduce_concat=[2, 3, 4, 5])
 
 DARTS = DARTS_V2
+
+
+BATH = Genotype(normal=[('max_pool_3x3', 0), ('max_pool_3x3', 1), ('max_pool_3x3', 0), ('sep_conv_5x5', 2), ('dil_conv_5x5', 0), ('max_pool_3x3', 2), ('sep_conv_3x3', 2), ('sep_conv_3x3', 0)], normal_concat=range(2, 6), reduce=[('max_pool_3x3', 1), ('max_pool_3x3', 0), ('max_pool_3x3', 1), ('sep_conv_5x5', 2), ('skip_connect', 3), ('avg_pool_3x3', 2), ('sep_conv_3x3', 4), ('dil_conv_5x5', 1)], reduce_concat=range(2, 6))
