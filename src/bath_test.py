2c27fe4b9abcab7f84089f1959c408be9a7a7218
k8lion <kaitlinmaile@gmail.com>
2021-02-24 11:21:08 +0100

    eval bath

---
+++ src/bath_test.py
@@ -0,0 +1,98 @@
+import argparse
+import logging
+import sys
+
+import numpy as np
+import torch
+import torch.backends.cudnn as cudnn
+import torch.nn as nn
+import torch.utils
+from torch.autograd import Variable
+
+import utils
+from model import NetworkBathy as Network
+
+parser = argparse.ArgumentParser("cifar")
+parser.add_argument('--data', type=str, default='../data', help='location of the data corpus')
+parser.add_argument('--batch_size', type=int, default=96, help='batch size')
+parser.add_argument('--report_freq', type=float, default=50, help='report frequency')
+parser.add_argument('--gpu', type=int, default=0, help='gpu device id')
+parser.add_argument('--init_channels', type=int, default=36, help='num of init channels')
+parser.add_argument('--layers', type=int, default=20, help='total number of layers')
+parser.add_argument('--model_path', type=str, default='EXP/model.pt', help='path of pretrained model')
+parser.add_argument('--auxiliary', action='store_true', default=False, help='use auxiliary tower')
+parser.add_argument('--cutout', action='store_true', default=False, help='use cutout')
+parser.add_argument('--cutout_length', type=int, default=16, help='cutout length')
+parser.add_argument('--drop_path_prob', type=float, default=0.0, help='drop path probability')
+parser.add_argument('--seed', type=int, default=0, help='random seed')
+parser.add_argument('--arch', type=str, default='BATH', help='which architecture to use')
+parser.add_argument('--min_energy', type=float, default=0.1, help='minimum energy')
+parser.add_argument('--max_energy', type=float, default=4.0, help='maximum energy')
+parser.add_argument('--max_depth', type=float, default=40.0, help='maximum unnormalized depth')
+
+args = parser.parse_args()
+
+log_format = '%(asctime)s %(message)s'
+logging.basicConfig(stream=sys.stdout, level=logging.INFO,
+                    format=log_format, datefmt='%m/%d %I:%M:%S %p')
+
+
+def main():
+    if not torch.cuda.is_available():
+        logging.info('no gpu device available')
+        sys.exit(1)
+
+    np.random.seed(args.seed)
+    torch.cuda.set_device(args.gpu)
+    cudnn.benchmark = True
+    torch.manual_seed(args.seed)
+    cudnn.enabled = True
+    torch.cuda.manual_seed(args.seed)
+    logging.info('gpu device = %d' % args.gpu)
+    logging.info("args = %s", args)
+
+    genotype = eval("genotypes.%s" % args.arch)
+    model = Network(args.init_channels, 1, args.layers, args.auxiliary, genotype, input_channels=4)
+    model = model.cuda()
+    utils.load(model, args.model_path)
+
+    logging.info("param size = %fMB", utils.count_parameters_in_MB(model))
+
+    criterion = nn.MSELoss()
+    criterion = criterion.cuda()
+
+    test_data = utils.BathymetryDataset(args, "../29TNE.csv", root_dir="dataset/bathymetry/29TNE/dataset_29TNE", to_trim="/tmp/pbs.6233542.admin01/tmp_portugal/")
+
+    test_queue = torch.utils.data.DataLoader(
+        test_data, batch_size=args.batch_size, shuffle=False, pin_memory=True, num_workers=2)
+
+    model.drop_path_prob = args.drop_path_prob
+    test_acc, test_obj = infer(test_queue, model, criterion)
+    logging.info('test_acc %f', test_acc)
+
+
+def infer(test_queue, model, criterion):
+    objs = utils.AverageMeter()
+    top1 = utils.AverageMeter()
+    model.eval()
+
+    for step, (input, target) in enumerate(test_queue):
+        input = Variable(input).cuda()
+        target = Variable(target).cuda(non_blonking=True)
+
+        logits, _ = model(input)
+        loss = criterion(logits, target)
+
+        prec1, prec5 = utils.accuracy(logits, target, topk=(1, ))
+        n = input.size(0)
+        objs.update(loss.item(), n)
+        top1.update(prec1[0].item(), n)
+
+        if step % args.report_freq == 0:
+            logging.info('test %03d %e %f', step, objs.avg, top1.avg)
+
+    return top1.avg, objs.avg
+
+
+if __name__ == '__main__':
+    main()
