6dc88687e57f06e2e21ca97f8faeb91fbfdd903d
k8lion <kaitlinmaile@gmail.com>
2021-03-01 14:23:40 +0100

    remove shuffle

--- src/train_scheduled.py
+++ src/train_scheduled.py
@@ -97,7 +97,7 @@ def main():
         pin_memory=True, num_workers=2)
 
     valid_queue = torch.utils.data.DataLoader(
-        train_data, batch_size=args.batch_size, shuffle=True,
+        train_data, batch_size=args.batch_size,
         sampler=torch.utils.data.sampler.SubsetRandomSampler(indices[split:num_train]),
         pin_memory=True, num_workers=2)
 
