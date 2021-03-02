59d907ea9d30e26bd91fe5b575bcac63aa84f36e
k8lion <kaitlinmaile@gmail.com>
2021-03-02 16:51:24 +0100

    try plotting

--- src/utils.py
+++ src/utils.py
@@ -227,8 +227,7 @@ def save_file(recoder, path='./', steps=None):
         try:
             axs[dest, src+2].plot(steps, v, label=op, color=COLORMAP[op])
         except:
-            print(len(steps), len(v))
-            axs[dest, src + 2].plot(v, label=op, color=COLORMAP[op])
+            axs[dest, src + 2].plot([0]+steps, v, label=op, color=COLORMAP[op])
         if "none" in op:
             has_none = True
     for i in range(0, 3):
