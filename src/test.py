22e3d0abf9debf1c4819d824e6edd832da43fbb7
k8lion <kaitlinmaile@gmail.com>
2021-03-08 13:25:30 +0100

    default arg

--- src/test.py
+++ src/test.py
@@ -15,7 +15,7 @@ import utils
 from model import NetworkCIFAR as Network
 
 parser = argparse.ArgumentParser("cifar")
-parser.add_argument('--data', type=str, default='../data', help='location of the data corpus')
+parser.add_argument('--data', type=str, default='dataset', help='location of the data corpus')
 parser.add_argument('--batch_size', type=int, default=96, help='batch size')
 parser.add_argument('--report_freq', type=float, default=50, help='report frequency')
 parser.add_argument('--gpu', type=int, default=0, help='gpu device id')
